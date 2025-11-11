938175d2fde6831f9a2f528acd35308e5e702255
AhmedHafez32 <mohahz38556@gmai.com>
2025-11-11 16:16:38 +0200

    auth: add add SignUp page UI with reusable form fields (SignUpForm and custom text fields)

--- lib/features/auth/view/signUpPage.dart
+++ lib/features/auth/view/signUpPage.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hungry/core/constants/appAssets.dart';
 import 'package:hungry/core/constants/appColors.dart';
-import 'package:hungry/core/routing/app_routes.dart';
-import 'package:hungry/core/sheared/customButtonWidget.dart';
-import 'package:hungry/core/sheared/customTextFormField.dart';
 import 'package:hungry/core/sheared/spacing_widgets.dart';
 import 'package:hungry/core/styles/appStyles.dart';
 
 import '../widgets/sign_up_form.dart';
 
-class SignUpPage extends StatefulWidget {
+class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
 
-  @override
-  State<SignUpPage> createState() => _SignUpPageState();
-}
-
-class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
